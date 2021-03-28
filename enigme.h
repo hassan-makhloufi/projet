37f815bead05c4a9ef3c6e5d9a9ab7c3b7d71418
projet <hassan.makhloufi@esprit.tn>
2021-03-29 01:22:24 +0200

    hsan

---
+++ enigme.h
@@ -0,0 +1,12 @@
+#ifndef QUIZ_H_
+#define QUIZ_H_
+
+
+
+void enigma(SDL_Surface *ecran,int d);
+void reponse(SDL_Surface *ecran,int d);
+void solution(SDL_Surface *ecran,int d);
+void correct(SDL_Surface *ecran);
+
+
+#endif
---
+++ main.c
@@ -0,0 +1,36 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <SDL/SDL.h>
+#include <SDL/SDL_image.h>
+#include <SDL/SDL_mixer.h>
+#include "enigme.h"
+#include <SDL/SDL_ttf.h>
+#include <time.h>
+int main()
+{
+
+
+int d=1;
+
+srand(time(NULL));
+d=rand()%7;
+SDL_Surface *backg=NULL ; //declaration des variables globale
+SDL_Rect positiond;
+SDL_Init(SDL_INIT_VIDEO); // Initialisation de la SDL
+SDL_Surface *ecran =NULL;
+ecran=SDL_SetVideoMode(800, 600, 32,SDL_HWSURFACE  |  SDL_DOUBLEBUF); // Ouverture de la fenêtre
+backg = IMG_Load("enigme.png");
+positiond.x=0 ;
+positiond.y=0 ;
+SDL_BlitSurface(backg,NULL, ecran, &positiond);
+SDL_Flip(ecran);
+SDL_Delay(1000);
+enigma(ecran,d);
+SDL_Flip(ecran);
+reponse(ecran,d);
+SDL_Flip(ecran);
+SDL_FreeSurface(backg);
+SDL_Quit(); // Arrêt de la SL
+
+    return EXIT_SUCCESS; // Fermeture du programme
+}
